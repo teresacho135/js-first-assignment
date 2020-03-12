2c01be167a126f47fac4ca404b3a975bde74398e
Teresa Cho <tcho@cca.edu>
2020-03-12 13:26:08 -0700

    completes question 1 in switch section

--- app.js
+++ app.js
@@ -129,3 +129,34 @@ function gradeMe() {
     };
   document.getElementById('response-display').innerHTML = response;
 };
+
+// Assignment 2: Switch
+
+// 1. Write a program to prompt the user for age. 
+// If age is equal to 14, alert "coupon 1". 
+// If age is equal to 21, alert "coupon 2". 
+// If age is equal to 30, alert "coupon 3". 
+// If age is equal to 50, alert "coupon 4". 
+// Else alert "No coupon".
+
+function freeCoupons() {
+  let age = prompt('Enter your age please');
+  switch (age) {
+    case '14':
+      alert('coupon 1');
+      break;
+    case '21':
+      alert('coupon 2');
+      break;
+    case '30':
+      alert('coupon 3');
+      break;
+    case '50':
+      alert('coupon 4')
+      break;
+    default:
+      alert('no coupon for you');
+      break;
+  };
+};
+
