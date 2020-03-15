d3d299b8397c6293753952583f78acac4fd0565a
Teresa Cho <tcho@cca.edu>
2020-03-15 16:58:09 -0700

    completes question 7 in assignment 5

--- app.js
+++ app.js
@@ -676,3 +676,17 @@ const toLowerCase = (string) => {
 
 // toLowerCase('HELLO');
 
+// 7. Consider the string “I like this product; 
+// I hate this product; This product is worth buying; 
+// I don’t agree with the above user”.
+//  Now break this whole string into pieces wherever you see 
+//  “;”. Now print the broken down pieces one after the other in console.
+
+const splitString = (string) => {
+  splitted = string.split(';')
+  console.log(splitted);
+  return splitted;
+};
+
+// splitString('I like this product; I hate this product; This product is worth buying; I don’t agree with the above user');
+
