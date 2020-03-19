640b18cb1b0db3f3e627146d41917bf18ebf8f42
Teresa Cho <tcho@cca.edu>
2020-03-18 21:58:23 -0700

    skips question 10 section b and question 11 because of dead link

--- app.js
+++ app.js
@@ -710,4 +710,39 @@ const countOccurance = (char) => {
  const stringTwo = 'Hello, my name is Teresa';
 
  const replaced = stringTwo.replace('name', 'javascript');
- console.log(replaced);
+//  console.log(replaced);
+
+//  10. Find the number of times the string "not" appears in this sentence -
+// a. " Javascript notation %^&* notes that structure rules and not context for nothing"
+// b. Now, for the above problem 4, create an array with texts before and after "not" occurrences.
+// (a).
+const stringThree = 'Javascript notation %^&* notes that structure rules and not context for nothing'
+
+const wordOccurance = (word) => {
+  let count = 0;
+  let splittedStrings = stringThree.split(' ');
+  for(let i = 0; i < splittedStrings.length; i++) {
+    if(splittedStrings[i] === word) {
+      count++
+    };
+  };
+  return count;
+};
+
+// console.log(wordOccurance('not'));
+
+// (b). Confused w/ problem brief-does this mean that we return string when taking out the word 'not' or after it has been counted how many times 'not' has occurred in string. 
+
+// 11. There is a link: http://www.pro-tekconsulting.com/main/navigate/about&us/contactus/email/your
+// info.jsp
+// a. Alert- ‘contact us’
+// b. Alert- ‘your info’
+// c. Have two text fields- Replace ‘navigate’ , ‘contact’ with whatever the user enters in the text
+// field and hits ‘submit’
+// d. Have 3 text fields- whatever you enter in those text fields should replace ‘prompt1’,’prompt2’
+// and prompt3’.Then ‘alert’ that. After 3rd alert, the whole new link should come up.
+// http://www.pro-tekconsulting.com/main/navigate/about&us/contactus/email/
+// firstname=prompt1 & lastname=prompt&ph_no=prompt3”
+
+//Link is dead.
+
