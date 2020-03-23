b3235228e59a13c10c51a763ba18923b6ce44cc3
Teresa Cho <tcho@cca.edu>
2020-03-23 15:42:14 -0700

    completes question 1; in assignment7

---
+++ assignment-7.js
@@ -0,0 +1,6 @@
+//--------------------------------------------------Assignment 7: DOM----------------------------------------------//
+// 1. Have a paragraph. On click of a button, alert the text in the paragraph.
+
+function initiateParagraph() {
+  document.getElementById('paragraph').innerHTML = 'ALERT: Here is a paragraph. '
+};
