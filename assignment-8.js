ae6852d2dbbf77b236a493e48f162fc7df4fe42e
Teresa Cho <tcho@cca.edu>
2020-04-01 21:34:54 -0700

    adds prompt on assignment-7

---
+++ assignment-8.js
@@ -0,0 +1,8 @@
+//--------------------------------------------------Assignment 7: DOM----------------------------------------------//
+
+//Form Validation
+
+// Create a form with First Name, Last Name, radio buttons for Gender 
+// (Male and Female), drop down menu for State,
+//  a check box for I ACCEPT and a submit button.
+//   At the click of submit button, the following should be validated.
