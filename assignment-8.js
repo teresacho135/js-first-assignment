d2abbba56822ce5d87b9631e5051b44afa864988
Teresa Cho <tcho@cca.edu>
2020-04-01 23:17:49 -0700

    moves on to question 2

--- assignment-8.js
+++ assignment-8.js
@@ -6,3 +6,8 @@
 // (Male and Female), drop down menu for State,
 //  a check box for I ACCEPT and a submit button.
 //   At the click of submit button, the following should be validated.
+
+//check index.html
+
+// 1. If the First Name and Last Name fields are left blank or
+//  has a number, the background color of the text box should change to red color.
