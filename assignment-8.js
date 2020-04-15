b35e32004b1936573f79eab360d74a86375a0c8a
Teresa Cho <tcho@cca.edu>
2020-04-15 15:36:57 -0700

    completes form validation for question 1 w/ regex checking for letters - no numbers

--- assignment-8.js
+++ assignment-8.js
@@ -12,32 +12,19 @@
 // 1. If the First Name and Last Name fields are left blank or
 //  has a number, the background color of the text box should change to red color.
 
-// const validateForm = () => {
-//   debugger;
-//   let firstName = document.forms['info-form']['first-name'];
-//   let lastName = document.forms['info-form']['last-name'];
-//   if (firstName.value === '') {
-//     // alert('this is empty');
-// debugger;
-//     document.getElementById('first-name').style.backgroundColor = 'red';
-//     return false;
-//   };
-// };
-//check other FIRST NAME IDS!
-// const validateForm = (e) => {
-//   e.preventDefault();
-//   return alert('this has been activated')
-// }
 
-//cannot seem to get validateForm() to ouput. 
-//Has CSS psuedo-selector that can create output but struggling on logic. 
+
 
 const validateForm = (event) => {
-  let firstName = document.forms['info-form']['first-name'];
-  let lastName = document.forms['info-form']['last-name'];
-  if(firstName.value === '' && lastName.value === '' ) {
+  let firstName = document.forms['info-form']['first-name'].value;
+  let lastName = document.forms['info-form']['last-name'].value;
+  let letters = /^[A-Za-z]+$/;
+  if(firstName === '' && lastName === '' ) {
     document.getElementById('first-name-info').style.backgroundColor = 'red';
     document.getElementById('last-name-info').style.backgroundColor = 'red';
-  }
+  } else if(!firstName.match(letters) && !lastName.match(letters)) {
+    document.getElementById('first-name-info').style.backgroundColor = 'red';
+    document.getElementById('last-name-info').style.backgroundColor = 'red';
+  };
   event.preventDefault();
-}
+};
