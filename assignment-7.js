6d4af2d3c24d916b04fb8f27a0fe8fee0ed4d44e
Teresa Cho <tcho@cca.edu>
2020-04-17 16:47:17 -0700

    creates html for creating a fahrenheit calculator to celcius

--- assignment-7.js
+++ assignment-7.js
@@ -125,3 +125,16 @@ const validateForm = (event) => {
 
 // 4. If the check box is not selected, alert the user to check the box.
 //refer back to validateForm() function.
+
+//DOM and EVENTS:
+// 1. Write a program to convert Fahrenheit temperature to Celsius. 
+// Here are the things that needs to be implemented as a part of this question:
+// a. Ask the input from the user for Fahrenheit using a text field
+// b. As soon as the user types in the Fahrenheit number in the text field, 
+// the result should display immediately in the div below
+// c. Hence, there should not be any submit button
+// d. Hint: Use onkeyup event. Formula is : C = (F - 32) * 5/9
+// e. Don't display the decimal values - but you should accept decimal values from the user
+// f. If the user enters any invalid values (strings) ) 
+// then you should make the input box border red and prompt user to enter correct values
+
